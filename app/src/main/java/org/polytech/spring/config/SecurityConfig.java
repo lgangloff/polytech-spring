c2e87085d645e3d4c5b30b3d9b3d1e5bd1f8b90e
Loic G <loic.gangloff@gmail.com>
2024-04-12 11:14:43 +0200

    secu

--- app/src/main/java/org/polytech/spring/config/SecurityConfig.java
+++ app/src/main/java/org/polytech/spring/config/SecurityConfig.java
@@ -4,7 +4,9 @@ import javax.sql.DataSource;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -14,14 +16,20 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity(jsr250Enabled = true)
 public class SecurityConfig {
 
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 		http
+			.csrf((csrf) -> csrf.disable())
 			.authorizeHttpRequests((authorize) -> authorize
+				.requestMatchers(HttpMethod.DELETE).hasRole("ADMIN")
+				.requestMatchers("/doctors").hasAnyRole("ADMIN")
+				.requestMatchers("/patient/**").hasAnyRole("USER")
 				.anyRequest().authenticated()
 			)
+			.httpBasic(Customizer.withDefaults())
 			.formLogin(Customizer.withDefaults());
 
 		return http.build();
--- app/src/main/java/org/polytech/spring/services/PatientService.java
+++ app/src/main/java/org/polytech/spring/services/PatientService.java
@@ -5,10 +5,15 @@ import java.util.List;
 import org.polytech.spring.patient.Patient;
 import org.polytech.spring.patient.PatientNotFoundException;
 import org.polytech.spring.repository.PatientRepository;
+import org.polytech.spring.security.IsCurrentPatient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
+import org.springframework.security.access.prepost.PostAuthorize;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
+
+import jakarta.annotation.security.RolesAllowed;
 
 @Service
 @Scope("singleton")
@@ -20,19 +25,23 @@ public class PatientService {
     public Patient save(Patient patient){
         return patientRepository.save(patient);
     }
-
-    public Patient update(Integer id, Patient updatedPatient) throws PatientNotFoundException{
-        return patientRepository.save(updatedPatient);
-    }
-
+    
+    
     public void delete(Integer id) throws PatientNotFoundException{
         patientRepository.deleteById(id);
     }
 
+    @IsCurrentPatient
     public Patient findOneById(Integer id) throws PatientNotFoundException {
         return patientRepository.findById(id).orElseThrow(PatientNotFoundException::new);
     }
-    
+
+    @PreAuthorize("hasRole('ADMIN')")
+    public Patient update(Integer id, Patient updatedPatient) throws PatientNotFoundException{
+        return patientRepository.save(updatedPatient);
+    }
+
+    @RolesAllowed("ADMIN")    
     public List<Patient> findAllByNameLike(String name) {
         return findAll().stream()
             .filter(p->StringUtils.startsWithIgnoreCase(p.getFirstName(), name))
